2f2ccbb5f30d93ecb7a43553ae48f3d06489fa31
Acemcshlaghg <s012456@students.lmsd.org>
2020-02-20 10:38:05 -0500

    Initial Commit

---
+++ Cortex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cortex
+//
+//  Created by Daniel Rosen (student LM) on 2/20/20.
+//  Copyright © 2020 Daniel Rosen (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
