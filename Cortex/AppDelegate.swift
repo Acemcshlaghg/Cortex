7809433bb838d5fa0f27ea6fa873f21750f9178c
Acemcshlaghg <s012456@students.lmsd.org>
2020-02-21 12:18:30 -0500

    Change to programmable, added HomeScreenViewController

--- Cortex/AppDelegate.swift
+++ Cortex/AppDelegate.swift
@@ -12,11 +12,14 @@ import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure();
+        window = UIWindow(frame: UIScreen.main.bounds);
+        window?.rootViewController = HomeScreenViewController();
+        window?.makeKeyAndVisible();
         return true
     }
 
