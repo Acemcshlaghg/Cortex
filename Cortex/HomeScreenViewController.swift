1f4285c889f09e1e81cf4a8e0c842de7da9fcf1c
Omribr <Omri.brenner@gmail.com>
2020-02-24 13:42:26 -0500

    change to HomeScreenViewController

--- Cortex/HomeScreenViewController.swift
+++ Cortex/HomeScreenViewController.swift
@@ -9,7 +9,46 @@
 import UIKit
 
 class HomeScreenViewController: UIViewController {
+    
     let label = UILabel();
+    let logInButton = UIButton()
+    let SignUpButton = UIButton()
+
+    func setUpLogInButton(){
+        view.addSubview(logInButton)
+        
+        logInButton.setTitle("Log In", for: .normal)
+        logInButton.setTitleColor(.black, for: .normal)
+        logInButton.backgroundColor = .white
+        logInButton.titleLabel?.font = UIFont(name: "Helvetica", size: 20)
+        logInButton.addTarget(self, action: #selector(goToLogInViewController), for:.touchUpInside)
+        
+        // constrains
+        logInButton.translatesAutoresizingMaskIntoConstraints = false
+        logInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        logInButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 200).isActive = true
+        logInButton.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        logInButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+    }
+    
+    func setUpSignUpButton(){
+        view.addSubview(SignUpButton)
+
+        SignUpButton.setTitle("Sign Up", for: .normal)
+        SignUpButton.setTitleColor(.black, for: .normal)
+        SignUpButton.backgroundColor = .white
+        SignUpButton.titleLabel?.font = UIFont(name: "Helvetica", size: 20)
+        SignUpButton.addTarget(self, action: #selector(goToSignUpViewController), for:.touchUpInside)
+        
+        // constrains
+        SignUpButton.translatesAutoresizingMaskIntoConstraints = false
+        SignUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        SignUpButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 100).isActive = true
+        SignUpButton.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        SignUpButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
     func setUpLabel(){
            view.addSubview(label);
 
@@ -23,12 +62,22 @@ class HomeScreenViewController: UIViewController {
            label.heightAnchor.constraint(equalToConstant: 300).isActive = true;
        }
     
+    @objc func goToLogInViewController(){
+        
+    }
+    
+    @objc func goToSignUpViewController(){
+        
+    }
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         view.backgroundColor = UIColor.white;
         print("working")
         setUpLabel();
+        setUpSignUpButton()
+        setUpLogInButton()
         // Do any additional setup after loading the view.
     }
     
