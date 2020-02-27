d7b067e6d92a8dfcc776b835b10fb579a96b5ed1
Acemcshlaghg <s012456@students.lmsd.org>
2020-02-26 21:39:36 -0500

    Sign Up VC

--- Cortex/SignUpViewController.swift
+++ Cortex/SignUpViewController.swift
@@ -16,7 +16,56 @@ class SignUpViewController: UIViewController {
     let emailAddressTextField = UITextField();
     let passwordTextField = UITextField();
     
-    @IBAction func signUpButtonPressed(_ sender: Any) {
+    func setUpTextFields(){
+        view.addSubview(userNameTextField);
+        userNameTextField.translatesAutoresizingMaskIntoConstraints = false;
+        userNameTextField.backgroundColor = .white;
+        userNameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true;
+        userNameTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true;
+        userNameTextField.widthAnchor.constraint(equalToConstant: 200).isActive = true;
+        userNameTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true;
+        userNameTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        userNameTextField.placeholder = "Username";
+        
+        view.addSubview(emailAddressTextField);
+        emailAddressTextField.translatesAutoresizingMaskIntoConstraints = false;
+        emailAddressTextField.backgroundColor = .white;
+        emailAddressTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true;
+        emailAddressTextField.centerYAnchor.constraint(equalTo: userNameTextField.bottomAnchor, constant: 100).isActive = true;
+        emailAddressTextField.widthAnchor.constraint(equalToConstant: 200).isActive = true;
+        emailAddressTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true;
+        emailAddressTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        emailAddressTextField.placeholder = "Email";
+        
+        view.addSubview(passwordTextField);
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false;
+        passwordTextField.backgroundColor = .white;
+        passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true;
+        passwordTextField.topAnchor.constraint(equalTo: emailAddressTextField.bottomAnchor, constant: 100).isActive = true;
+        passwordTextField.widthAnchor.constraint(equalToConstant: 200).isActive = true;
+        passwordTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true;
+        passwordTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        passwordTextField.placeholder = "Password";
+
+    }
+    
+    func setUpButton(){
+              //add button to view
+              view.addSubview(signUpButton);
+              
+              //set-up properties
+              signUpButton.backgroundColor = UIColor.blue;
+              signUpButton.setTitle("Sign Up", for: .normal);
+              signUpButton.setTitleColor(.white, for: .normal);
+              signUpButton.titleLabel?.font = UIFont(name: "Times", size: 40);
+              signUpButton.addTarget(self, action: #selector(signUpButtonPressed), for: .touchUpInside);
+              signUpButton.translatesAutoresizingMaskIntoConstraints = false;
+              signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true;
+              signUpButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10).isActive = true;
+              signUpButton.widthAnchor.constraint(equalToConstant: 300).isActive = true;
+              signUpButton.heightAnchor.constraint(equalToConstant: 100).isActive = true;
+          }
+    @objc func signUpButtonPressed(_ sender: Any) {
         
         print("pressed");
         guard let email = emailAddressTextField.text else {return};
@@ -40,24 +89,6 @@ class SignUpViewController: UIViewController {
         }
     }
        
-       func setUpButton(){
-           //add button to view
-           view.addSubview(signUpButton);
-           
-           //set-up properties
-           signUpButton.backgroundColor = UIColor.black;
-           signUpButton.setTitle("Button", for: .normal);
-           signUpButton.setTitleColor(.red, for: .normal);
-           signUpButton.titleLabel?.font = UIFont(name: "Times", size: 40);
-           signUpButton.addTarget(self, action: #selector(changeText), for: .touchUpInside);
-           
-           signUpButton.translatesAutoresizingMaskIntoConstraints = false;
-           signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true;
-           signUpButton.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: 10).isActive = true;
-           
-           signUpButton.widthAnchor.constraint(equalToConstant: 300).isActive = true;
-           signUpButton.heightAnchor.constraint(equalToConstant: 100).isActive = true;
-       }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool{
         if(userNameTextField.isFirstResponder){
             emailAddressTextField.becomeFirstResponder();
@@ -71,7 +102,9 @@ class SignUpViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .purple;
+        setUpButton();
+        setUpTextFields();
         // Do any additional setup after loading the view.
     }
     
