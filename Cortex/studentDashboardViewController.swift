69c877e205a598335de4fc34e95f962b6b9c0006
Ben Warren <s017274@students.lmsd.org>
2020-02-27 12:32:45 -0500

    Changed the constraints and properties of the Student Dashboard to match the Tutor Dashboard

--- Cortex/studentDashboardViewController.swift
+++ Cortex/studentDashboardViewController.swift
@@ -14,7 +14,7 @@
 
 
 
-import Foundation
+import Foundations
 
 import UIKit
 
@@ -22,7 +22,9 @@ import UIKit
 
 class studentDashboardViewController: UIViewController {
     
-    
+    let settingsButton = UIButton()
+
+    let titleLabel = UILabel()
     
     let findATutorButton = UIButton()
     
@@ -39,6 +41,19 @@ class studentDashboardViewController: UIViewController {
     
     
     
+    func setUpLabel(){
+        view.addSubview(titleLabel)
+        
+        // set up the properties
+        titleLabel.text = "Tutor Dashboard"
+        titleLabel.textColor = UIColor.black
+        titleLabel.font = UIFont(name: "Helvetica", size: 25)
+        
+        // constraints
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalToSystemSpacingAfter: view.centerXAnchor, multiplier: 0).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: findATutorButton.topAnchor, constant: -50).isActive = true
+    }
     func setUpButtons(){
         
         //        Adds all the buttons to the view
@@ -53,63 +68,71 @@ class studentDashboardViewController: UIViewController {
         
         view.addSubview(switchToTutorButton)
         
+        view.addSubview(settingsButton)
+        
         //        Set up button properties
         
         //        Find a tutor
         
-        findATutorButton.setTitle("Find a Tutor", for: .normal)
-        
-        findATutorButton.setTitleColor(.black, for: .normal)
-        
-        findATutorButton.backgroundColor = .lightGray
-        
-        findATutorButton.titleLabel?.font = UIFont(name: "Papyrus", size: 35)
+        // set up properties
+             findATutorButton.titleLabel?.numberOfLines = 0
+             // set up the properties
+             findATutorButton.setTitle("Tutor \nRequests" , for: .normal)
+             findATutorButton.setTitleColor(.green, for: .normal)
+             findATutorButton.backgroundColor = .black
+             findATutorButton.titleLabel?.font = UIFont(name: "Helvetica", size: 30)
+            findATutorButton.titleLabel?.textAlignment = .center
         
         findATutorButton.addTarget(self, action: #selector(findATutorAction), for: .touchUpInside)
         
         //        Calendar
         
-        calendarButton.setTitle("Calendar", for: .normal)
-        
-        calendarButton.setTitleColor(.black, for: .normal)
-        
-        calendarButton.backgroundColor = .lightGray
-        
-        calendarButton.titleLabel?.font = UIFont(name: "Papyrus", size: 35)
+       // set up properties
+               calendarButton.setTitle("Calendar" , for: .normal)
+               calendarButton.setTitleColor(.yellow, for: .normal)
+               calendarButton.backgroundColor = .black
+               calendarButton.titleLabel?.font = UIFont(name: "Helvetica", size: 30)
+               calendarButton.titleLabel?.textAlignment = .center
+               
         
         //   calendarButton.addTarget(self, action: #selector(calendarAction), for: .touchUpInside)
         
         //        Profile
         
-        profileButton.setTitle("Profile", for: .normal)
-        
-        profileButton.setTitleColor(.black, for: .normal)
-        
-        profileButton.backgroundColor = .lightGray
-        
-        profileButton.titleLabel?.font = UIFont(name: "Papyrus", size: 35)
+         // set up properties
+              profileButton.titleLabel?.numberOfLines = 0
+              profileButton.setTitle("Profile \nPage" , for: .normal)
+              profileButton.setTitleColor(.red, for: .normal)
+              profileButton.backgroundColor = .black
+              profileButton.titleLabel?.font = UIFont(name: "Helvetica", size: 30)
+              profileButton.titleLabel?.textAlignment = .center
         
         //  profileButton.addTarget(self, action: #selector(profileAction), for: .touchUpInside)
         
         //        Study Groups
         
-        studyGroupsButton.setTitle("Study Groups", for: .normal)
-        
-        studyGroupsButton.setTitleColor(.black, for: .normal)
-        
-        studyGroupsButton.backgroundColor = .lightGray
-        
-        studyGroupsButton.titleLabel?.font = UIFont(name: "Papyrus", size: 35)
+      // set up properties
+              studyGroupsButton.titleLabel?.numberOfLines = 0
+              studyGroupsButton.setTitle("Study \nGroup" , for: .normal)
+              studyGroupsButton.setTitleColor(.blue, for: .normal)
+              studyGroupsButton.backgroundColor = .black
+              studyGroupsButton.titleLabel?.font = UIFont(name: "Helvetica", size: 30)
+              studyGroupsButton.titleLabel?.textAlignment = .center
         
         //       studyGroupsButton.addTarget(self, action: #selector(studyGroupAction), for: .touchUpInside)
         
-        switchToTutorButton.setTitle("Switch to Tutor Account", for: .normal)
+ switchToTutorButton.titleLabel?.numberOfLines = 0
+        switchToTutorButton.setTitle("Switch To \nStudent Account", for: .normal)
+        switchToTutorButton.setTitleColor(UIColor.cyan, for: .normal)
+        switchToTutorButton.backgroundColor = .black
+        switchToTutorButton.titleLabel?.font = UIFont(name: "Helvetica", size: 30)
+        switchToTutorButton.titleLabel?.textAlignment = .center
         
-        switchToTutorButton.setTitleColor(.black, for: .normal)
-        
-        switchToTutorButton.backgroundColor = .lightGray
-        
-        switchToTutorButton.titleLabel?.font = UIFont(name: "Papyrus", size: 35)
+//        Settings
+        settingsButton.setTitle("Settings" , for: .normal)
+               settingsButton.setTitleColor(.black, for: .normal)
+               settingsButton.backgroundColor = .white
+               settingsButton.titleLabel?.font = UIFont(name: "Helvetica", size: 20)
         
         
         //        Set constraints
@@ -119,31 +142,50 @@ class studentDashboardViewController: UIViewController {
         profileButton.translatesAutoresizingMaskIntoConstraints = false
         studyGroupsButton.translatesAutoresizingMaskIntoConstraints = false
         switchToTutorButton.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        settingsButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+//       Title Label
+    
+        titleLabel.centerXAnchor.constraint(equalToSystemSpacingAfter: view.centerXAnchor, multiplier: 0).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: findATutorButton.topAnchor, constant: -50).isActive = true
         
         //        Find a tutor button
-        findATutorButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        findATutorButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100).isActive = true
+        findATutorButton.centerXAnchor.constraint(equalTo:view.centerXAnchor, constant: -95).isActive = true
+        findATutorButton.centerYAnchor.constraint(equalTo:view.centerYAnchor, constant: -80).isActive = true
+        findATutorButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        findATutorButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
         
         //      Calendar Button
         
-        calendarButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        calendarButton.leftAnchor.constraint(equalTo: findATutorButton.rightAnchor, constant: 50).isActive = true
-        calendarButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 100).isActive = true
+       calendarButton.centerXAnchor.constraint(equalTo:view.centerXAnchor, constant: 95).isActive = true
+        calendarButton.centerYAnchor.constraint(equalTo:view.centerYAnchor, constant: -80).isActive = true
+        calendarButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        calendarButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
         
         //        Profile Button
-        profileButton.topAnchor.constraint(equalTo: findATutorButton.bottomAnchor, constant: 50).isActive = true
-        profileButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100).isActive = true
-        profileButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 100).isActive = true
+       profileButton.centerXAnchor.constraint(equalTo:view.centerXAnchor, constant: -95).isActive = true
+        profileButton.centerYAnchor.constraint(equalTo:view.centerYAnchor, constant: 80).isActive = true
+        profileButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        profileButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
         
         //        Study Groups Button
-        studyGroupsButton.topAnchor.constraint(equalTo: calendarButton.bottomAnchor, constant: 50).isActive = true
-        studyGroupsButton.leftAnchor.constraint(equalTo: profileButton.rightAnchor, constant: 50).isActive = true
-        studyGroupsButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 100).isActive = true
-        studyGroupsButton.bottomAnchor.constraint(equalTo: switchToTutorButton.topAnchor, constant: 100).isActive = true
-//        Switch to student button
+        studyGroupsButton.centerXAnchor.constraint(equalTo:view.centerXAnchor, constant: 95).isActive = true
+        studyGroupsButton.centerYAnchor.constraint(equalTo:view.centerYAnchor, constant: 80).isActive = true
+        studyGroupsButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        studyGroupsButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        //        Switch to student button
         
-        switchToTutorButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        switchToTutorButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 50).isActive = true
+        switchToTutorButton.centerXAnchor.constraint(equalTo:view.centerXAnchor, constant: 0).isActive = true
+        switchToTutorButton.centerYAnchor.constraint(equalTo:profileButton.bottomAnchor, constant: 80).isActive = true
+        switchToTutorButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        switchToTutorButton.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        
+//      Settings Button
+        
+    settingsButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5).isActive = true
+        settingsButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 35).isActive = true
         
     }
     
